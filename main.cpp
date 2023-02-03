388ef48c153a00afc2a0bbb24b6cdb6517a24d5a
ifizmat <ifizmat@gmail.com>
2023-02-03 11:00:42 +0400

    4. Field color in class Pen.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 
 class Pen {
-	
+	public:
+		std::string color;
 };
 
 int main() {
 	using namespace std;
 	cout << "Test v4.011: OK.\n";
 	Pen pen;
+	pen.color = "#FFFFFF";
+	cout << "Color of pen: " << pen.color << endl;
 	return 0;
 }
