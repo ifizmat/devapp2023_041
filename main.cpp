c6d3d17f8f43cf0ce1176d5003aec4d0245d55de
ifizmat <ifizmat@gmail.com>
2023-02-03 11:34:00 +0400

    5. Prived _color, accessors.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 
 class Pen {
+	private:
+		std::string _color;
+		
 	public:
-		std::string color;
+		void setColor(std::string code);
+		std::string getColor() { return _color; }
 };
+
+void Pen::setColor(std::string code) {
+	_color = code;
+}
 
 int main() {
 	using namespace std;
 	cout << "Test v4.011: OK.\n";
 	Pen pen;
-	pen.color = "#FFFFFF";
-	cout << "Color of pen: " << pen.color << endl;
+	//pen._color = "#FFFFFF";
+	pen.setColor("#FFFFFF");
+	cout << "Color of pen: " << pen.getColor() << endl;
+	//cout << "Color of pen: " << pen._color << endl;
 	return 0;
 }
